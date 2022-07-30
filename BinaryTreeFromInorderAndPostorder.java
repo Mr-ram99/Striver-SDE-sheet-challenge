bc2a7f29cc2667e63486fdc564ac9de47e8fe786
Mr-ram9 <jrchoudhary2410@gmail.com>
2022-07-30 11:18:45 +0530

    Day56

--- BinaryTreeFromInorderAndPostorder.java
+++ BinaryTreeFromInorderAndPostorder.java
@@ -1,44 +1,37 @@
 /************************************************************
-   
+  
    Following is the TreeNode class structure
    
-   class TreeNode<T>
-   { 
-   public:
+   class TreeNode<T> { 
         T data; 
-        TreeNode<T> *left;
-        TreeNode<T> *right;
-   
-        TreeNode(T data) 
-  		{ 
-            this -> data = data; 
-            left = NULL; 
-            right = NULL; 
+        TreeNode<T> left;
+        TreeNode<T> right;
+  
+        TreeNode(T data) { 
+            this.data = data; 
+            left = null; 
+            right = null; 
         }
-   };
-   
-   
+   }
+    
  ************************************************************/
-#include <bits/stdc++.h>
-map<int, int> mp;
-TreeNode<int>* BT(vector<int> &postorder, int postStart, int postEnd, vector<int> &inorder, int inStart, int inEnd)
-{
-    if(postStart > postEnd || inStart > inEnd) return NULL;
-    
-    TreeNode<int> *root = new TreeNode<int>(postorder[postEnd]);
-    int idx = mp[postorder[postEnd]];
-    int leftNums = idx - inStart;
-    
-    root->left = BT(postorder, postStart, postStart+leftNums-1, inorder, inStart, idx-1);
-    root->right = BT(postorder, postStart+leftNums, postEnd-1, inorder, idx+1, inEnd);
-    
-    return root;    
-}
-TreeNode<int>* getTreeFromPostorderAndInorder(vector<int>& postOrder, vector<int>& inOrder) 
-{
-    // Write your code here.
-    for(int i = 0; i < inOrder.size(); i++)
-        mp[inOrder[i]] = i;
-    
-    return BT(postOrder, 0, postOrder.size()-1, inOrder, 0, inOrder.size()-1);
+import java.util.*;
+public class Solution {
+	public static TreeNode<Integer> getTreeFromPostorderAndInorder(int[] postOrder, int[] inOrder) {
+		// Write your code here.
+        int n = postOrder.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++) map.put(inOrder[i],i);
+        return build(postOrder, 0, n-1, inOrder, 0, n-1, map);
+	}
+
+    public static TreeNode build(int[] post, int ps, int pe, int[] in, int is, int ie, HashMap<Integer, Integer> map){
+        if(is>ie || ps>pe) return null;
+        TreeNode node = new TreeNode(post[pe]);
+        int index = map.get(post[pe]);
+        int right = ie-index;
+            node.right = build(post, pe-right, pe-1, in, index+1, ie, map);
+            node.left = build(post, ps, pe-right-1, in, is, index-1, map);
+        return node;
+    }
 }
